b152aee5973be9eebb8bc909404ce873100bbf55
Pranshu Bahadur <pranshu041@gmail.com>
2021-05-02 22:44:16 +0530

    test

--- modules.py
+++ modules.py
@@ -190,10 +190,10 @@ class Net(nn.Module):
         self.head = nn.Conv2d(in_channels=3,out_channels=8,kernel_size=3, stride=1)
         self.swish = MemoryEfficientSwish()
         self.bn = BatchNormalization2D(8)
-        self.channels = [8, 16, 32, 64]
+        self.channels = [8, 16, 32, 64, 128]
         self.stages = nn.ModuleList([nn.Sequential(
-        MBConv(n, n*2, 1, 2, dp, 18), BatchNormalization2D(n*2), MemoryEfficientSwish(),
-        MBConv(n*2, n*2, 3, 1, dp, 18), BatchNormalization2D(n*2), MemoryEfficientSwish(),
+        MBConv(n, n*2, 1, 2, dp, 24), BatchNormalization2D(n*2), MemoryEfficientSwish(),
+        #MBConv(n*2, n*2, 3, 1, dp, 18), BatchNormalization2D(n*2), MemoryEfficientSwish(),
         #MBConv(n*2, n*2, 3, 1, dp, 6), BatchNormalization2D(n*2), MemoryEfficientSwish(),
         #MBConv(n*2, n*2, 3, 1, dp, 6), BatchNormalization2D(n*2), MemoryEfficientSwish(),
         #MBConv(n*2, n*2, 3, 1, dp, 6), BatchNormalization2D(n*2), MemoryEfficientSwish(),
