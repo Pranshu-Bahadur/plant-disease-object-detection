2072e4d7c518d3c39dfc38fd0a71b89b347e88cc
Pranshu Bahadur <pranshu041@gmail.com>
2021-05-03 01:59:56 +0530

    ranaug

--- experiment.py
+++ experiment.py
@@ -51,7 +51,10 @@ class Experiment(object):
         """
         transformations = [
             transforms.Resize([resolution, resolution], PIL.Image.ANTIALIAS),
+            RandAugment(),
+            RandAugment(),
             #RandAugment(),
+            transforms.Grayscale(3),
             transforms.ToTensor(),
             #transforms.Normalize(mean=mean_sum, std=std_sum)
         ]
