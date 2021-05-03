54529b19fdbbc9fb078f5ca616f6b2c6fa3147b0
Pranshu Bahadur <pranshu041@gmail.com>
2021-05-03 08:48:43 +0530

    test

--- modules.py
+++ modules.py
@@ -187,17 +187,17 @@ class Net(nn.Module):
     def __init__(self, nc, dp=0):
         super(Net, self).__init__()
         self.init_batch_norm = BatchNormalization2D(3)
-        self.head = nn.Conv2d(in_channels=3,out_channels=128,kernel_size=3, stride=4)
+        self.head = nn.Conv2d(in_channels=3,out_channels=128,kernel_size=3, stride=2)
         self.swish = MemoryEfficientSwish()
         self.bn = BatchNormalization2D(128)
-        self.channels = [128, 512]
+        self.channels = [128]
         self.stages = nn.ModuleList([nn.Sequential(
-        MBConv(n, n, 3, 2, dp, 6), BatchNormalization2D(n), MemoryEfficientSwish(),
-        MBConv(n, n, 3, 1, dp, 6), BatchNormalization2D(n), MemoryEfficientSwish(),
+        MBConv(n, n, 3, 2, dp, 18), BatchNormalization2D(n), MemoryEfficientSwish(),
+        MBConv(n, n, 3, 1, dp, 18), BatchNormalization2D(n), MemoryEfficientSwish(),
         #MBConv(n, n, 3, 1, dp, 6), BatchNormalization2D(n), MemoryEfficientSwish(),
         #MBConv(n, n, 3, 1, dp, 6), BatchNormalization2D(n), MemoryEfficientSwish(),
         #MBConv(n, n, 3, 1, dp, 6), BatchNormalization2D(n), MemoryEfficientSwish(),
-        MBConv(n, n*4, 3, 2, dp, 6), BatchNormalization2D(n*4), MemoryEfficientSwish(),
+        MBConv(n, n*4, 3, 2, dp, 18), BatchNormalization2D(n*4), MemoryEfficientSwish(),
         #MBConv(n*2, n*2, 3, 2, dp, 6), BatchNormalization2D(n*2), MemoryEfficientSwish(),
         #MBConv(n*2, n*2, 3, 2, dp, 6), BatchNormalization2D(n*2), MemoryEfficientSwish(),
         #MBConv(n*2, n*2, 3, 2, dp, 6), BatchNormalization2D(n*2), MemoryEfficientSwish(),
