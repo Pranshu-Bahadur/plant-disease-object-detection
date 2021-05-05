6b84b3ccc17ae58f86d261dc4568156d482868b1
Pranshu Bahadur <pranshu041@gmail.com>
2021-05-05 14:34:13 +0530

    test

--- modules.py
+++ modules.py
@@ -210,9 +210,9 @@ class Net(nn.Module):
         for i in range(len(self.config)):
             for j in range(self.config[i][2]):
                 self.stages[i].add_module(str(j+1), MBConv(self.config[i][0], self.config[i][0], 3, 1, 0, self.config[i][4]) if j is not self.config[i][2] - 1 else MBConv(self.config[i][0], self.config[i][1], 3, self.config[i][3], 0, self.config[i][4]))
-        self.final_conv = nn.Sequential(nn.Conv2d(self.config[-1][1], self.config[-1][1]*4, 1, 1))#, BatchNormalization2D(self.config[-1][1]), MemoryEfficientSwish())
+        self.final_conv = nn.Sequential(nn.Conv2d(self.config[-1][1], self.config[-1][1], 1, 1))#, BatchNormalization2D(self.config[-1][1]), MemoryEfficientSwish())
         self.gap = nn.AdaptiveAvgPool2d(1)
-        self.fc = nn.Linear(self.config[-1][1]*4, nc)
+        self.fc = nn.Linear(self.config[-1][1], nc)
     def forward(self, x):
         x = self.head(x)
         x = self.bn(x)
@@ -221,6 +221,6 @@ class Net(nn.Module):
             x = stage(x)
         x = self.final_conv(x)
         x = self.gap(x)
-        x = x.view(-1, self.config[-1][1]*4)
+        x = x.view(-1, self.config[-1][1])
         x = self.fc(x)
         return x
