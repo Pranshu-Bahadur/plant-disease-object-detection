8334d8cf5a4235b007aa6662078242ae598433fe
Pranshu Bahadur <pranshu041@gmail.com>
2021-05-06 11:54:39 +0530

    test

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ if __name__ == "__main__":
     if args.opMode:
         transformations = [
             transforms.Resize([int(args.resolution), int(args.resolution)], PIL.Image.ANTIALIAS),
-            transforms.Grayscale(1),
+            transforms.Grayscale(3),
             transforms.ToTensor(),
         ]
         transformations = transforms.Compose(transformations)
