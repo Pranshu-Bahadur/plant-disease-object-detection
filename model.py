ec3e3ef7e91bde3680596bd954590ae19dd8bfb7
Pranshu Bahadur <pranshu041@gmail.com>
2021-05-09 13:59:23 +0530

    test

--- model.py
+++ model.py
@@ -100,7 +100,7 @@ class ImageClassifier(object):
             x, y = data
             total += y.size(0)
             if train:
-                x = torch.stack([torchvision.transforms.ToTensor()(self.RA_Helper(torchvision.transforms.Resize(self.resolution - 32*self.counter,interpolation=PIL.Image.ANTIALIAS)(torchvision.transforms.ToPILImage()(img)), self.counter)) for img in x])
+                x = torch.stack([torchvision.transforms.ToTensor()(self.RA_Helper(torchvision.transforms.Resize(self.resolution - 64*self.counter,interpolation=PIL.Image.ANTIALIAS)(torchvision.transforms.ToPILImage()(img)), self.counter)) for img in x])
                 print(x[0].size())
                 if type(self.optimizer) == SAMSGD:
                     def closure():
