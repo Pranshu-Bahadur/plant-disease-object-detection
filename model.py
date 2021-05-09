cf2d961501138ded908766327738a5e1644cfb64
Pranshu Bahadur <pranshu041@gmail.com>
2021-05-09 11:58:11 +0530

    test

--- model.py
+++ model.py
@@ -50,7 +50,7 @@ class ImageClassifier(object):
     def _create_optimizer(self, name, model_params, lr):
         optim_dict = {"SGD":torch.optim.SGD(model_params.parameters(), lr,weight_decay=1e-5, momentum=0.9, nesterov=True),
                       "SAMSGD": SAMSGD(model_params.parameters(), lr, momentum=0.9,weight_decay=1e-5),
-                      "SGDAGC": SGD_AGC(model_params.parameters(), lr=lr, momentum=0.9, nesterov=True, clipping=0.01, weight_decay=1e-5)
+                      "SGDAGC": SGD_AGC(model_params.parameters(), lr=lr, momentum=0.9, nesterov=True, clipping=0.32, weight_decay=1e-5)
         }
         return optim_dict[name]
     
