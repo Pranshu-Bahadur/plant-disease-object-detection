4d86f4ead0db73e3370348b0571f52ce96646dfd
Pranshu Bahadur <pranshu041@gmail.com>
2021-05-10 12:46:49 +0530

    test

--- model.py
+++ model.py
@@ -94,7 +94,7 @@ class ImageClassifier(object):
         if train: #and (self.curr_epoch+1)%5==0:
             self.counter = max(self.counter - 1, 0)
             print("Changing resolution...")
-            #self.optimizer.param_groups[0]['clipping'] = self.optimizer.param_groups[0]['clipping']*2  if self.bs>16 or self.curr_epoch==self.final_epoch-2 else 5.12
+            self.optimizer.param_groups[0]['clipping'] = self.optimizer.param_groups[0]['clipping']*2  if self.bs>16 or self.curr_epoch==self.final_epoch-2 else 2.56
             self.bs = self.bs//2 if self.bs>16 or self.curr_epoch==self.final_epoch-2 else 16
         for idx, data in enumerate(loader):
             self.optimizer.zero_grad()
