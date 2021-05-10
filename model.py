978e028e8b8d9dc707336a2a9fce420045087a54
Pranshu Bahadur <pranshu041@gmail.com>
2021-05-10 11:40:16 +0530

    test

--- model.py
+++ model.py
@@ -33,7 +33,7 @@ class ImageClassifier(object):
         self.writer = SummaryWriter(log_dir="logs/{}".format(self.name))
         self.writer.flush()
         self.resolution = config["resolution"]
-        self.counter = 4
+        self.counter = 3
         self.final_epoch = config["epochs"]
         print("Generated model: {}".format(self.name))
 
@@ -191,9 +191,9 @@ class ImageClassifier(object):
 
     def RA_Helper(self, x, i, j, idx):
         if idx==0:
-            torchvision.utils.save_image(torchvision.transforms.ToTensor()(x[0]), "/home/fraulty/ws/RP_TBX11K/content/{}_Before_RA_{}_{}.png".format(self.curr_epoch+1,self.resolution - 32*self.counter, j))
+            torchvision.utils.save_image(torchvision.transforms.ToTensor()(x), "/home/fraulty/ws/RP_TBX11K/content/{}_Before_RA_{}_{}.png".format(self.curr_epoch+1,self.resolution - 32*self.counter, j))
         for _ in range(3 - i):
             x = RandAugment()(x)
         if idx==0:
-            torchvision.utils.save_image(torchvision.transforms.ToTensor()(x[0]), "/home/fraulty/ws/RP_TBX11K/content/{}_After_RA_{}_{}.png".format(self.curr_epoch+1,self.resolution - 32*self.counter, j))
+            torchvision.utils.save_image(torchvision.transforms.ToTensor()(x), "/home/fraulty/ws/RP_TBX11K/content/{}_After_RA_{}_{}.png".format(self.curr_epoch+1,self.resolution - 32*self.counter, j))
         return x
