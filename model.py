a995ac5d7fcbf0a76e53661387e79135b82a2e41
Pranshu Bahadur <pranshu041@gmail.com>
2021-05-11 15:52:39 +0530

    test

--- model.py
+++ model.py
@@ -103,6 +103,7 @@ class ImageClassifier(object):
                 x_, y_ = [], []
                 for i in range(3):
                     if x[y==i].size(0) > 0:
+                        """
                         if i==2:
                             for _ in range(4):
                                 if self.counter > 0:
@@ -111,8 +112,9 @@ class ImageClassifier(object):
                                 else:
                                     x_.append(torch.stack([torchvision.transforms.ToTensor()(self.RA_Helper(torchvision.transforms.ToPILImage()(img), self.counter, i, idx)) for img in x[y==i]]))
                                     y_.append(y[y==i])
-                        else:
-                        #for _ in range(4):
+                        """
+                        #else:
+                        for _ in range(4):
                             if self.counter > 0:
                                 x_.append(torch.stack([torchvision.transforms.ToTensor()(self.RA_Helper(torchvision.transforms.Resize(self.resolution - 32*self.counter,interpolation=PIL.Image.ANTIALIAS)(torchvision.transforms.ToPILImage()(img)), self.counter, i, idx)) for img in x[y==i]]))
                                 y_.append(y[y==i])
