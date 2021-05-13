5b60f49281724ee8909563ce85c4c1082ddeee5b
Pranshu Bahadur <pranshu041@gmail.com>
2021-05-14 01:55:35 +0530

    test

--- model.py
+++ model.py
@@ -127,8 +127,8 @@ class ImageClassifier(object):
                 x_ = torch.cat(x_, dim=0)
                 """
                 #shuffle_seed = torch.randperm(x.size(0))
-                list(map(lambda img: torchvision.transforms.ToTensor()(self.RA_Helper(torchvision.transforms.Resize(self.resolution - 32*self.counter)(torchvision.transforms.ToPILImage()(img)), self.counter, 0, idx)), x)) if self.counter > 0 else list(map(lambda img: torchvision.transforms.ToTensor()(self.RA_Helper(torchvision.transforms.ToPILImage()(img), self.counter, 0, idx)), x))
-                #x = torch.stack(x)
+                x = list(map(lambda img: torchvision.transforms.ToTensor()(self.RA_Helper(torchvision.transforms.Resize(self.resolution - 32*self.counter)(torchvision.transforms.ToPILImage()(img)), self.counter, 0, idx)), x)) if self.counter > 0 else list(map(lambda img: torchvision.transforms.ToTensor()(self.RA_Helper(torchvision.transforms.ToPILImage()(img), self.counter, 0, idx)), x))
+                x = torch.tensor(x)
                 #x = x[shuffle_seed]
                 #y = y[shuffle_seed]#torch.cat(y_, dim=0)
                 total += y.size(0)
