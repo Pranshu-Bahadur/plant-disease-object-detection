6a43b6f66ab7c6b136d307ee795624191210c4ab
Pranshu Bahadur <pranshu041@gmail.com>
2021-05-14 08:02:27 +0530

    res

--- model.py
+++ model.py
@@ -87,12 +87,15 @@ class ImageClassifier(object):
                 ]
             if self.counter-1 > 0:
                 transforms.insert(1, torchvision.transforms.Resize(self.resolution - 32*(self.counter-1)))
+                
             for i in range(4-self.counter):
                 if i < 2:
                     transforms.insert(1, RandAugment())
             transforms = torchvision.transforms.Compose(transforms)
 
             split[0] = list(map(lambda x: (torch.stack(list(map(lambda img: transforms(img), x[0]))),x[1]) ,list(split[0])))
+        if self.curr_epoch==2:
+            split[0] = list(map(lambda x: (torchvision.transforms.functional.resize(x[0], self.resolution),x[1]) ,list(split[0])))
         #print(len(split[0]), print(split[0][0].size()))
         train_acc, train_loss = self._train_or_eval(split[0], True)
         self.model.eval()
